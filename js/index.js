d5b409b26f1f0726a400b8f18c781e398d90a458
devthigas <thi.deusefiel@gmail.com>
2023-09-12 13:48:45 -0300

    menu mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import accordionInit from "./modules/accordion.js";
 import tabMenuInit from "./modules/tabmenu.js";
+import menuMobileInit from "./modules/menuMobile.js";
 
 tabMenuInit()
 accordionInit()
+menuMobileInit()
---
+++ js/modules/menuMobile.js
@@ -0,0 +1,17 @@
+export default function menuMobileInit(){
+    const toggle = document.querySelector('.toggle')
+    const menuList = document.querySelector('.js-list')
+
+    if(toggle && menuList){
+        function activeMenu(){
+            menuList.classList.toggle('active')
+            const toggleIcon = toggle.querySelector('span')
+            toggleIcon.classList.toggle('active')
+            toggleIcon.innerText = toggleIcon.innerText === 'menu' ? 'close' : 'menu'
+        }
+
+        toggle.addEventListener('click', activeMenu)
+
+    }
+    
+}
